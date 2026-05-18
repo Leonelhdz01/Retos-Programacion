140fe60f0efb4aab71ee309869c6e0e8276e2898
Leonel <leohdz37@icloud.com>
2026-05-17 19:48:21 -0500

    feat: solve anamgrama exercise in java

---
+++ Java/Ejercicio02.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Ejercicio02 {
+    public static void main(String[] args) {
+
+
+        //palabras a comparar (roma, amor, mora: si son anagranmas, ropa no es anagrama)
+        String palabra1 = "ropa";
+        String palabra2 = "mora";
+
+        //si son exactamente iguales NO es un anagrama
+        if (palabra1.equals(palabra2)) {
+            System.out.println("No es un anagrama");
+        } else {
+
+            //convertimos las palabras en arrays de letras
+            char[] letras1 = palabra1.toCharArray();
+            char[] letras2 = palabra2.toCharArray();
+
+
+            //ordenamos las letras
+            Arrays.sort(letras1);
+            Arrays.sort(letras2);
+
+            //comparamos los arrays
+            if (Arrays.equals(letras1, letras2)) {
+                System.out.println("Si es un anagrama");
+            } else {
+                System.out.println("No es un anagrama");
+            }
+
+        }
+    }
+}
